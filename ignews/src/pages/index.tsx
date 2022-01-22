121bafe014847909e830b7a490e2b54b5d8aaea5
joao ribeiro <joao.ribeiro@havan.com.br>
2022-01-22 18:04:17 -0300

    estrututa da aplicação - estilos globais do app

---
+++ ignews/src/pages/index.tsx
@@ -0,0 +1,15 @@
+//import "../styles/global.scss";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>
+        hello <span>Mundo</span>
+      </h1>
+    </>
+  );
+}
